ca546d05c92fe4d810e58bef285764b37f5993d5
Nickolas <kolzazanovikov@gmail.com>
2024-06-14 22:27:46 +0700

    add test

--- test/copy_with_isar_test.dart
+++ test/copy_with_isar_test.dart
@@ -7,45 +7,44 @@ void main() {
   group('CopyWithGenerator', () {
     test('generates copyWith method correctly', () async {
       const source = '''
-      import 'package:copy_with_generator.dart';
-      
-      part 'user_model.i_copy.dart';
-
-      @CopyIsar()
-      class UserModel {
-        Id id = Isar.autoIncrement;
-
-        String? firstName;
-        String? lastName;
-      }
+        import 'package:copy_with_isar/annotations.dart';
+        
+        part 'user_model.icopy.dart';
+        
+        @IsarCopy()
+        class ExampleModel {
+          String? firstName;
+          String? lastName;
+        }
       ''';
 
       const expectedOutput = '''
-      // GENERATED CODE - DO NOT MODIFY BY HAND
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-      part of 'user_model.dart';
+part of 'user_model.dart';
 
-      extension UserModelCopyWith on UserModel {
-        UserModel copyWith({
-          Id? id,
-          String? firstName,
-          String? lastName,
-        }) {
-          return UserModel(
-            id: id ?? this.id,
-            firstName: firstName ?? this.firstName,
-            lastName: lastName ?? this.lastName,
-          );
-        }
-      }
-      ''';
+// **************************************************************************
+// CopyWithGenerator
+// **************************************************************************
 
-      final builder = PartBuilder([CopyWithGenerator()], '.i_copy.dart');
+extension ExampleModelCopyWith on ExampleModel {
+  ExampleModel copyWith({
+    String? firstName,
+    String? lastName,
+  }) {
+    return ExampleModel()
+      ..firstName = firstName ?? this.firstName
+      ..lastName = lastName ?? this.lastName;
+  }
+}
+''';
+
+      final builder = PartBuilder([CopyWithGenerator()], '.icopy.dart');
 
       await testBuilder(
         builder,
         {'a|lib/user_model.dart': source},
-        outputs: {'a|lib/user_model.copy_with.dart': expectedOutput},
+        outputs: {'a|lib/user_model.icopy.dart': expectedOutput},
         reader: await PackageAssetReader.currentIsolate(),
       );
     });
